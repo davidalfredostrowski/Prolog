0b11c789cd0781328d4522348da14d8b21538ef9
davidalfredostrowski <davidaostrowski@gmail.com>
2022-11-17 06:38:41 -0500

    Create test4.pl

---
+++ test4.pl
@@ -0,0 +1,17 @@
+bird(laysan_albatross):-
+family(albatross),
+color(white).
+bird(black_footed_albatross):-
+family(albatross),
+color(dark).
+bird(whistling_swan):-
+family(swan),
+voice(muffled_musical_whistle).
+bird(tumpeter_swan):-
+family(swan),
+voice(load_trumpeting).
+
+
+
+family(albatross).
+color(dark).
