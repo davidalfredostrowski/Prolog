c8e8b6a0403b98d021d060a653e413697bca6c1c
davidalfredostrowski <davidaostrowski@gmail.com>
2022-11-29 13:23:35 -0500

    Create test6.pl

---
+++ test6.pl
@@ -0,0 +1,11 @@
+
+located_in(X, usa) :- located_in(X,georgia).
+located_in(X, usa) :- located_in(X,colorado).
+located_in(X, canada) :- located_in(X, ontario).
+located_in(X, north_america) :- located_in(X, usa).
+located_in(X, north_america) :- located_in(X, canada).
+
+located_in(atlanta, georgia).
+located_in(denver, colorado).
+located_in(boulder, colorado).
+located_in(toronto, ontario).
